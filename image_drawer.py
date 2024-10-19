a6d872080a983072f34fe800c45258af0c7d85e2
m-h3sh <157107806+m-h3sh@users.noreply.github.com>
2024-10-19 14:31:19 +0530

    Add files via upload

--- image_drawer.py
+++ image_drawer.py
@@ -8,6 +8,7 @@ import numpy as np
 from tf2_ros.buffer import Buffer 
 from tf2_ros.transform_listener import TransformListener
 from cv_bridge import CvBridge
+DEBUG = 0
 
 lp = lg.LaserProjection()
 
@@ -15,53 +16,48 @@ class image_plotter(Node):
     def __init__(self):
         self.jugad = 0
         super().__init__('imageplotter')
-        self.scan_subscriber = self.create_subscription(PointCloud2, '/igvc/ipm', self.ipm_callback, 100)
-        self.points = []
+        self.scan_subscriber_right = self.create_subscription(PointCloud2, '/ipm_right', self.ipm_callback_right, 100)
+        self.scan_subscriber_left = self.create_subscription(PointCloud2, '/ipm_left', self.ipm_callback_left, 100)
         self.leftpoints = []
         self.rightpoints = []
         self.buffer = Buffer()
         self.bridge = CvBridge()
         self.drawer_publisher = self.create_publisher(Image, '/lanes_draw', 1)
+        self.img = np.zeros((1200, 1200, 3), dtype=np.uint8)
+        self.rightimg = np.zeros((1200, 1200, 3), dtype=np.uint8)
+        self.leftimg = np.zeros((1200, 1200, 3), dtype=np.uint8)
         
         #self.camera_1_listener = self.buffer.lookup_transform('odom', self.camera_1_id, rclpy.time.Time())
         #self.camera_2_listener = self.buffer.lookup_transform('odom', self.camera_2_id, rclpy.time.Time())
 
         #self.tf_listener = TransformListener(self.buffer, self)
 
-    def ipm_callback(self, pc2_msg):
-        frame = pc2_msg.header.frame_id
-        if self.jugad: 
-            self.leftpoints = []
-        else:
-            self.rightpoints = []
+    def ipm_callback_right(self, pc2_msg):
+        self.rightimg = np.zeros((1200, 1200, 3), dtype=np.uint8)
+        self.rightpoints = []
         data = ros2_numpy.numpify(pc2_msg)
         for point in data['xyz']:
-            if self.jugad:
-                self.leftpoints.append([(point[0]+6)*100 , (point[1]+6)*100, 0]);
-            else:
-                self.rightpoints.append([(point[0]+6)*100 , (point[1]+6)*100, 0]);
+            self.rightpoints.append([(point[0]+6)*100 , (point[1]+6)*100, 0])
+        for point in self.rightpoints:
+            self.rightimg = cv2.circle(self.rightimg, (int(point[0]),int(point[1])), 1, (255, 255, 255), -1)
 
-        self.points = self.leftpoints
-        self.points.extend(self.rightpoints)
-        
-        #maxx = max([i[0] for i in points])
-        #maxy = max([i[1] for i in points])
-        #minx = min([i[0] for i in points])
-        #miny = min([i[1] for i in points])
-        #img = np.zeros((int(maxy-miny+10),int(maxx-minx+10) , 3), dtype=np.uint8)
-        img = np.zeros((1200, 1200, 3), dtype=np.uint8)
-        for point in self.points:
-            img = cv2.circle(img, (int(point[0]),int(point[1])), 1, (255, 255, 255), -1)
-        # img = cv2.circle(img, (int(max([i[0] for i in points])),int(max([i[1] for i in points]))), 10, (0, 0, 255), -1)
-        # img = cv2.circle(img, (int(min([i[0] for i in points])),int(min([i[1] for i in points]))), 10, (0, 0, 255), -1)
-        #print("hi2")
-        cv2.imshow("trial",img)
-        cv2.waitKey(1)
-        imgmessage = self.bridge.cv2_to_imgmsg(img, "rgb8")
+    def ipm_callback_left(self, pc2_msg1):
+        self.leftimg = np.zeros((1200, 1200, 3), dtype=np.uint8)
+        self.leftpoints = []
+        data1 = ros2_numpy.numpify(pc2_msg1)
+        for point1 in data1['xyz']:
+            self.leftpoints.append([(point1[0]+6)*100 , (point1[1]+6)*100, 0])
+        for point1 in self.leftpoints:
+            self.leftimg = cv2.circle(self.leftimg, (int(point1[0]),int(point1[1])), 1, (255, 255, 255), -1)
+
+        # Publishing the image
+        self.img = cv2.bitwise_or(self.leftimg, self.rightimg)
+        imgmessage = self.bridge.cv2_to_imgmsg(self.img, "rgb8")
         self.drawer_publisher.publish(imgmessage)
 
-        if self.jugad: self.jugad = 0
-        else: self.jugad = 1
+        if (DEBUG):
+            cv2.imshow("ipm data",self.img)
+            cv2.waitKey(1)
 
 def main(args=None):
     rclpy.init(args=args)
